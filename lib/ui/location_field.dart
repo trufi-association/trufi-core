6cb40948a16da3983425897bdb87df80c365c284
Raimund Wege <raimundwege@gmail.com>
2018-08-17 14:54:06 +0200

    Added location fields

---
+++ lib/ui/location_field.dart
@@ -0,0 +1,182 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'package:map_view/map_view.dart';
+import 'package:map_view/polygon.dart';
+import 'package:map_view/polyline.dart';
+
+const API_KEY = "***REMOVED***";
+
+class LocationField extends StatefulWidget {
+  const LocationField({
+    this.fieldKey,
+    this.hintText,
+    this.labelText,
+    this.helperText,
+    this.onSaved,
+    this.validator,
+    this.onFieldSubmitted,
+  });
+
+  final Key fieldKey;
+  final String hintText;
+  final String labelText;
+  final String helperText;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
+  final ValueChanged<String> onFieldSubmitted;
+
+  @override
+  _LocationFieldState createState() => new _LocationFieldState();
+}
+
+class _LocationFieldState extends State<LocationField> {
+
+  MapView mapView = new MapView();
+  CameraPosition cameraPosition;
+  var compositeSubscription = new CompositeSubscription();
+  var staticMapProvider = new StaticMapProvider(API_KEY);
+  Uri staticMapUri;
+
+  @override
+  Widget build(BuildContext context) {
+    return new TextFormField(
+      key: widget.fieldKey,
+      maxLength: 8,
+      onSaved: widget.onSaved,
+      validator: widget.validator,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      decoration: new InputDecoration(
+        border: const UnderlineInputBorder(),
+        filled: true,
+        hintText: widget.hintText,
+        labelText: widget.labelText,
+        helperText: widget.helperText,
+        suffixIcon: new GestureDetector(
+          onTap: () {
+            showMap();
+          },
+          child: new Icon(Icons.add_location),
+        ),
+      ),
+    );
+  }
+
+  showMap() {
+    mapView.show(
+        new MapOptions(
+            mapViewType: MapViewType.normal,
+            showUserLocation: true,
+            showMyLocationButton: true,
+            showCompassButton: true,
+            initialCameraPosition: new CameraPosition(
+                new Location(45.526607443935724, -122.66731660813093), 15.0),
+            hideToolbar: false,
+            title: "Recently Visited"),
+        toolbarActions: [
+          new ToolbarAction("❌", 1),
+          new ToolbarAction("✔️", 2)
+        ]);
+    StreamSubscription sub = mapView.onMapReady.listen((_) {
+//      mapView.setMarkers(_markers);
+//      mapView.setPolylines(_lines);
+//      mapView.setPolygons(_polygons);
+    });
+    compositeSubscription.add(sub);
+    sub = mapView.onLocationUpdated.listen((location) {
+      print("Location updated $location");
+    });
+    compositeSubscription.add(sub);
+    sub = mapView.onTouchAnnotation
+        .listen((annotation) => print("annotation ${annotation.id} tapped"));
+    compositeSubscription.add(sub);
+    sub = mapView.onTouchPolyline
+        .listen((polyline) => print("polyline ${polyline.id} tapped"));
+    compositeSubscription.add(sub);
+    sub = mapView.onTouchPolygon
+        .listen((polygon) => print("polygon ${polygon.id} tapped"));
+    compositeSubscription.add(sub);
+    sub = mapView.onMapTapped
+        .listen((location) => print("Touched location $location"));
+    compositeSubscription.add(sub);
+    sub = mapView.onCameraChanged.listen((cameraPosition) =>
+        this.setState(() => this.cameraPosition = cameraPosition));
+    compositeSubscription.add(sub);
+    sub = mapView.onAnnotationDragStart.listen((markerMap) {
+      var marker = markerMap.keys.first;
+      print("Annotation ${marker.id} dragging started");
+    });
+    sub = mapView.onAnnotationDragEnd.listen((markerMap) {
+      var marker = markerMap.keys.first;
+      print("Annotation ${marker.id} dragging ended");
+    });
+    sub = mapView.onAnnotationDrag.listen((markerMap) {
+      var marker = markerMap.keys.first;
+      var location = markerMap[marker];
+      print("Annotation ${marker.id} moved to ${location.latitude} , ${location
+          .longitude}");
+    });
+    compositeSubscription.add(sub);
+    sub = mapView.onToolbarAction.listen((id) {
+      print("Toolbar button id = $id");
+      _handleDismiss(id != 1);
+    });
+    compositeSubscription.add(sub);
+    sub = mapView.onInfoWindowTapped.listen((marker) {
+      print("Info Window Tapped for ${marker.title}");
+    });
+    compositeSubscription.add(sub);
+  }
+
+  _handleDismiss(bool submit) async {
+    if (submit) {
+      double zoomLevel = await mapView.zoomLevel;
+      Location centerLocation = await mapView.centerLocation;
+      List<Marker> visibleAnnotations = await mapView.visibleAnnotations;
+      List<Polyline> visibleLines = await mapView.visiblePolyLines;
+      List<Polygon> visiblePolygons = await mapView.visiblePolygons;
+      print("Zoom Level: $zoomLevel");
+      print("Center: $centerLocation");
+      print("Visible Annotation Count: ${visibleAnnotations.length}");
+      print("Visible Polylines Count: ${visibleLines.length}");
+      print("Visible Polygons Count: ${visiblePolygons.length}");
+      var uri = await staticMapProvider.getImageUriFromMap(mapView,
+          width: 900, height: 400);
+      setState(() => staticMapUri = uri);
+    }
+    mapView.dismiss();
+    compositeSubscription.cancel();
+  }
+}
+
+class CompositeSubscription {
+  Set<StreamSubscription> _subscriptions = new Set();
+
+  void cancel() {
+    for (var n in this._subscriptions) {
+      n.cancel();
+    }
+    this._subscriptions = new Set();
+  }
+
+  void add(StreamSubscription subscription) {
+    this._subscriptions.add(subscription);
+  }
+
+  void addAll(Iterable<StreamSubscription> subs) {
+    _subscriptions.addAll(subs);
+  }
+
+  bool remove(StreamSubscription subscription) {
+    return this._subscriptions.remove(subscription);
+  }
+
+  bool contains(StreamSubscription subscription) {
+    return this._subscriptions.contains(subscription);
+  }
+
+  List<StreamSubscription> toList() {
+    return this._subscriptions.toList();
+  }
+}
