effe12a45c1ddbf179df93ce117617f9b743bf00
Raimund Wege <raimundwege@gmail.com>
2018-08-21 11:38:49 +0200

    Clean up

--- lib/ui/location_field.dart
+++ lib/ui/location_field.dart
@@ -8,16 +8,14 @@ import 'package:trufi_app/trufi_models.dart' as models;
 import 'package:trufi_app/ui/location_search_delegate.dart';
 
 class LocationField extends StatefulWidget {
-  const LocationField(
-      {this.fieldKey,
-      this.hintText,
-      this.labelText,
-      this.helperText,
-      this.onSaved,
-      this.validator,
-      this.onFieldSubmitted,
-      this.staticMapProvider,
-      this.mapView});
+  const LocationField({this.fieldKey,
+    this.hintText,
+    this.labelText,
+    this.helperText,
+    this.onSaved,
+    this.validator,
+    this.onFieldSubmitted,
+    this.mapView});
 
   final Key fieldKey;
   final String hintText;
@@ -26,7 +24,6 @@ class LocationField extends StatefulWidget {
   final FormFieldSetter<String> onSaved;
   final FormFieldValidator<String> validator;
   final ValueChanged<models.Location> onFieldSubmitted;
-  final StaticMapProvider staticMapProvider;
   final MapView mapView;
 
   @override
@@ -35,13 +32,10 @@ class LocationField extends StatefulWidget {
 
 class LocationFieldState extends State<LocationField> {
   final CompositeSubscription _compositeSubscription =
-      new CompositeSubscription();
-  final LocationSearchDelegate _delegate = new LocationSearchDelegate();
+  new CompositeSubscription();
+  final LocationSearchDelegate _searchDelegate = new LocationSearchDelegate();
   final FocusNode _focusNode = new FocusNode();
   final TextEditingController _textEditController = new TextEditingController();
-
-  CameraPosition cameraPosition;
-  Uri staticMapUri;
 
   models.Location location;
 
@@ -62,35 +56,32 @@ class LocationFieldState extends State<LocationField> {
   Widget build(BuildContext context) {
     return new Container(
         child: new Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-        new Expanded(
-            child: new TextFormField(
-          key: widget.fieldKey,
-          onSaved: widget.onSaved,
-          validator: widget.validator,
-          focusNode: _focusNode,
-          controller: _textEditController,
-          decoration: new InputDecoration(
-            border: const UnderlineInputBorder(),
-            filled: true,
-            hintText: widget.hintText,
-            labelText: widget.labelText,
-            helperText: widget.helperText,
-          ),
-        )),
-        new Center(
-            child: new IconButton(
-                icon: new Icon(Icons.add_location), onPressed: _showMap))
-      ],
-    ));
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new Expanded(
+                child: new TextFormField(
+                  key: widget.fieldKey,
+                  onSaved: widget.onSaved,
+                  validator: widget.validator,
+                  focusNode: _focusNode,
+                  controller: _textEditController,
+                  decoration: new InputDecoration(
+                    border: const UnderlineInputBorder(),
+                    filled: true,
+                    hintText: widget.hintText,
+                    labelText: widget.labelText,
+                    helperText: widget.helperText,
+                  ),
+                )),
+            new Center(
+                child: new IconButton(
+                    icon: new Icon(Icons.add_location), onPressed: _showMap))
+          ],
+        ));
   }
 
   _showSearch() async {
-    _setLocation(await showSearch(context: context, delegate: _delegate));
-    setState(() {
-      _textEditController.text = location?.description ?? "";
-    });
+    _setLocation(await showSearch(context: context, delegate: _searchDelegate));
   }
 
   Marker _positionMarker;
@@ -121,21 +112,18 @@ class LocationFieldState extends State<LocationField> {
       widget.mapView.setMarkers(<Marker>[_positionMarker]);
     });
     _compositeSubscription.add(sub);
-    sub = widget.mapView.onLocationUpdated.listen((location) {
-      print("Location updated $location");
-    });
     _compositeSubscription.add(sub);
+
+    // Replace marker on map tap
     sub = widget.mapView.onMapTapped.listen((location) {
       widget.mapView.removeMarker(_positionMarker);
       _positionMarker = _createMarker(location.latitude, location.longitude);
       widget.mapView.addMarker(_positionMarker);
     });
     _compositeSubscription.add(sub);
-    sub = widget.mapView.onCameraChanged.listen((cameraPosition) =>
-        this.setState(() => this.cameraPosition = cameraPosition));
-    _compositeSubscription.add(sub);
+
+    // React on toolbar buttons
     sub = widget.mapView.onToolbarAction.listen((id) {
-      print("Toolbar button id = $id");
       _hideMap(id != 1);
     });
     _compositeSubscription.add(sub);
@@ -159,22 +147,20 @@ class LocationFieldState extends State<LocationField> {
 
   _hideMap(bool submit) async {
     if (submit) {
-      Location centerLocation = await widget.mapView.centerLocation;
-      location = new models.Location(
-          description: centerLocation.toString(),
-          latitude: centerLocation.latitude,
-          longitude: centerLocation.longitude);
-      print("Center: $centerLocation");
-      var uri = await widget.staticMapProvider
-          .getImageUriFromMap(widget.mapView, width: 900, height: 400);
-      setState(() => staticMapUri = uri);
+      _setLocation(new models.Location(
+          description: "Marker Position",
+          latitude: _positionMarker.latitude,
+          longitude: _positionMarker.longitude)
+      );
     }
     widget.mapView.dismiss();
     _compositeSubscription.cancel();
   }
 
   _setLocation(models.Location location) {
-    this.location = location;
-    widget.onFieldSubmitted(location);
+    setState(() {
+      this.location = location;
+      _textEditController.text = location.description;
+    });
   }
 }
