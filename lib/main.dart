acc5a18a800e3b9393dff49155faef7ef43c906b
Raimund Wege <raimundwege@gmail.com>
2018-08-23 08:00:16 +0200

    Introduced location form field

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
-import 'package:composite_subscription/composite_subscription.dart';
 import 'package:flutter/material.dart';
 import 'package:map_view/map_view.dart';
 
 import 'package:trufi_app/trufi_api.dart';
 import 'package:trufi_app/trufi_map.dart';
 import 'package:trufi_app/trufi_models.dart';
-import 'package:trufi_app/ui/location_field.dart';
+import 'package:trufi_app/ui/location_form_field.dart';
 
 /// This API Key will be used for both the interactive maps as well as the static maps.
 /// Make sure that you have enabled the following APIs in the Google API Console (https://console.developers.google.com/apis)
@@ -27,10 +26,16 @@ class TrufiApp extends StatefulWidget {
 class PlanData {
   TrufiLocation fromPlace;
   TrufiLocation toPlace;
+  Plan plan;
+
+  reset() {
+    fromPlace = null;
+    toPlace = null;
+    plan = null;
+  }
 }
 
 class _TrufiAppState extends State<TrufiApp> {
-  final CompositeSubscription _subscription = new CompositeSubscription();
   final PlanData _planData = new PlanData();
   final MapView _mapView = new MapView();
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
@@ -40,8 +45,19 @@ class _TrufiAppState extends State<TrufiApp> {
     return new MaterialApp(
       home: new Scaffold(
           appBar: new AppBar(
-            title: new Text('Trufi'),
-          ),
+              title: new Text('Trufi'),
+              actions: _planData.toPlace != null
+                  ? <Widget>[
+                      new IconButton(
+                          icon: Icon(Icons.close),
+                          onPressed: () {
+                            _formKey.currentState.reset();
+                            setState(() {
+                              _planData.reset();
+                            });
+                          })
+                    ]
+                  : <Widget>[]),
           body: new Container(
               padding: new EdgeInsets.all(16.0),
               child: new Form(
@@ -49,27 +65,57 @@ class _TrufiAppState extends State<TrufiApp> {
                   child: new Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[
-                      new LocationField(
-                          helperText: 'Choose your start location.',
-                          labelText: 'Start',
-                          onSaved: (value) => _planData.fromPlace = value,
+                      _planData.toPlace != null
+                          ? new LocationFormField(
+                              helperText: 'Choose your origin location.',
+                              labelText: 'Origin',
+                              onSaved: (value) {
+                                print(value);
+                                setState(() {
+                                  _planData.fromPlace = value;
+                                  _fetchPlan();
+                                });
+                              },
+                              mapView: _mapView)
+                          : new Container(),
+                      new LocationFormField(
+                          helperText: 'Choose your destination.',
+                          labelText: 'Destination',
+                          onSaved: (value) {
+                            print(value);
+                            setState(() {
+                              _planData.toPlace = value;
+                              _fetchPlan();
+                            });
+                          },
                           mapView: _mapView),
-                      new LocationField(
-                          helperText: 'Choose your end location.',
-                          labelText: 'End',
-                          onSaved: (value) => _planData.toPlace = value,
-                          mapView: _mapView),
-                      new Expanded(child: new Container()),
-                      new Row(children: <Widget>[
-                        new Expanded(
-                            child: new RaisedButton(
-                                color: Colors.blue,
-                                onPressed: () => _submit(),
-                                child: const Text("Go for it")))
-                      ]),
+                      _planData.plan != null
+                          ? new Row(children: <Widget>[
+                              new Expanded(
+                                  child: new RaisedButton(
+                                      color: Colors.blue,
+                                      onPressed: () => _submit(),
+                                      child: const Text("Show on map")))
+                            ])
+                          : new Container(),
                     ],
                   )))),
     );
+  }
+
+  _fetchPlan() async {
+    if (_planData.toPlace != null) {
+      if (_planData.fromPlace == null) {
+        _planData.fromPlace = new TrufiLocation(
+            description: "Current Position",
+            latitude: -17.4603761,
+            longitude: -66.1860606);
+      }
+      Plan plan = await fetchPlan(_planData.fromPlace, _planData.toPlace);
+      setState(() {
+        _planData.plan = plan;
+      });
+    }
   }
 
   _submit() async {
---
+++ lib/ui/location_form_field.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:map_view/map_view.dart';
+import 'package:trufi_app/trufi_models.dart';
+import 'package:trufi_app/ui/location_map.dart';
+import 'package:trufi_app/ui/location_search_delegate.dart';
+
+class LocationFormField extends FormField<TrufiLocation> {
+  LocationFormField(
+      {FormFieldSetter<TrufiLocation> onSaved,
+      FormFieldValidator<TrufiLocation> validator,
+      TrufiLocation initialValue,
+      bool autovalidate = false,
+      String hintText,
+      String labelText,
+      String helperText,
+      MapView mapView})
+      : super(
+            onSaved: onSaved,
+            validator: validator,
+            initialValue: initialValue,
+            autovalidate: autovalidate,
+            builder: (FormFieldState<TrufiLocation> state) {
+              // Open location search
+              FocusNode focusNode = new FocusNode();
+              focusNode.addListener(() async {
+                if (focusNode.hasFocus) {
+                  focusNode.unfocus();
+                  state.didChange(await showSearch(
+                      context: state.context,
+                      delegate: new LocationSearchDelegate()));
+                  state.save();
+                }
+              });
+              return new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new Expanded(
+                      child: new TextField(
+                    focusNode: focusNode,
+                    controller: new TextEditingController(
+                        text: state.value?.description ?? ''),
+                    decoration: new InputDecoration(
+                      border: const UnderlineInputBorder(),
+                      filled: true,
+                      hintText: hintText,
+                      labelText: labelText,
+                      helperText: helperText,
+                    ),
+                  )),
+                  new Center(
+                      child: new IconButton(
+                          icon: new Icon(Icons.add_location),
+                          onPressed: () {
+                            LocationMap.create(mapView,
+                                onSubmit: (latitude, longitude) {
+                              state.didChange(new TrufiLocation(
+                                  description: "Marker Position",
+                                  latitude: latitude,
+                                  longitude: longitude));
+                              state.save();
+                            }).showMap();
+                          }))
+                ],
+              );
+            });
+}
