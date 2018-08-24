7dfcb3d29eb5156a2944d294b8067cd230135b78
Natalya Blanco <blanco@ubilabs.net>
2018-08-24 16:06:20 +0200

    refactors

--- lib/main.dart
+++ lib/main.dart
@@ -93,17 +93,7 @@ class _TrufiAppState extends State<TrufiApp>
         ),
         body: new Container(
           padding: new EdgeInsets.all(16.0),
-          child: _isPlanVisible()
-              ? new Row(children: <Widget>[
-                  new Expanded(
-                      child: _isPlanErrorVisible()
-                          ? new Text(plan.error.message)
-                          : new RaisedButton(
-                              color: const Color(0xffffd600),
-                              onPressed: () => _showMap(),
-                              child: const Text("Show on map")))
-                ])
-              : new Container(),
+          child: _buildPlan(),
         ),
       ),
     );
@@ -170,4 +160,108 @@ class _TrufiAppState extends State<TrufiApp>
     new TrufiMap.fromPlan(_mapView, await api.fetchPlan(fromPlace, toPlace))
         .showMap();
   }
+
+  Widget _buildPlan() {
+    PlanError error = plan?.error;
+    return new Container(
+      padding: new EdgeInsets.all(16.0),
+      child: error != null
+          ? _buildPlanFailure(error)
+          : plan != null ? _buildPlanSuccess(plan) : _buildPlanEmpty(),
+    );
+  }
+
+  Widget _buildPlanFailure(PlanError error) {
+    return new Container(child: new Text(error.message));
+  }
+
+  Widget _buildPlanSuccess(Plan plan) {
+    return new Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        new Row(children: <Widget>[
+          new Expanded(
+              child: new RaisedButton(
+                  color: const Color(0xffffd600),
+                  onPressed: () => _showMap(),
+                  child: const Text("Show on map")))
+        ]),
+      ],
+    );
+  }
+
+  Widget _buildPlanEmpty() {
+    return new Container();
+  }
 }
+
+// Displays one Entry. If the entry has children then it's displayed
+// with an ExpansionTile.
+class EntryItem extends StatelessWidget {
+  const EntryItem(this.entry);
+
+  final Entry entry;
+
+  Widget _buildTiles(Entry root) {
+    if (root.children.isEmpty) return ListTile(title: Text(root.title));
+    return ExpansionTile(
+      key: PageStorageKey<Entry>(root),
+      title: Text(root.title),
+      children: root.children.map(_buildTiles).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildTiles(entry);
+  }
+}
+
+// One entry in the multilevel list displayed by this app.
+class Entry {
+  Entry(this.title, [this.children = const <Entry>[]]);
+
+  final String title;
+  final List<Entry> children;
+}
+
+final List<Entry> data = <Entry>[
+  Entry(
+    'Chapter A',
+    <Entry>[
+      Entry(
+        'Section A0',
+        <Entry>[
+          Entry('Item A0.1'),
+          Entry('Item A0.2'),
+          Entry('Item A0.3'),
+        ],
+      ),
+      Entry('Section A1'),
+      Entry('Section A2'),
+    ],
+  ),
+  Entry(
+    'Chapter B',
+    <Entry>[
+      Entry('Section B0'),
+      Entry('Section B1'),
+    ],
+  ),
+  Entry(
+    'Chapter C',
+    <Entry>[
+      Entry('Section C0'),
+      Entry('Section C1'),
+      Entry(
+        'Section C2',
+        <Entry>[
+          Entry('Item C2.0'),
+          Entry('Item C2.1'),
+          Entry('Item C2.2'),
+          Entry('Item C2.3'),
+        ],
+      ),
+    ],
+  ),
+];
