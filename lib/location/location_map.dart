38da25bea052b85a07734b27aa803f8e952f9ba7
Raimund Wege <raimundwege@gmail.com>
2018-08-28 21:12:30 +0200

    Refactoring

--- lib/location/location_map.dart
+++ lib/location/location_map.dart
@@ -1,70 +1,48 @@
-import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:latlong/latlong.dart';
 
-import 'package:composite_subscription/composite_subscription.dart';
-import 'package:map_view/map_view.dart';
-import 'package:trufi_app/trufi_map_utils.dart';
+import 'package:trufi_app/location/location_map_controller.dart';
 
-typedef void OnSubmit(double latitude, double longitude);
-typedef void OnCancel();
+class ChooseOnMapScreen extends StatelessWidget {
+  final LatLng position;
 
-class LocationMap {
-  final MapView mapView;
-  final Marker locationMarker;
-  final OnSubmit onSubmit;
-  final OnCancel onCancel;
+  ChooseOnMapScreen({this.position});
 
-  final CompositeSubscription _subs = new CompositeSubscription();
-
-  LocationMap(this.mapView, this.locationMarker,
-      {this.onSubmit, this.onCancel});
-
-  factory LocationMap.create(MapView mapView,
-      {OnSubmit onSubmit, OnCancel onCancel}) {
-    return new LocationMap(mapView, createPositionMarker(-17.0, -66.0),
-        onSubmit: onSubmit, onCancel: onCancel);
-  }
-
-  showMap() {
-    mapView.show(
-        new MapOptions(
-            mapViewType: MapViewType.normal,
-            showUserLocation: true,
-            showMyLocationButton: true,
-            showCompassButton: true,
-            initialCameraPosition: new CameraPosition(
-                new Location(locationMarker.latitude, locationMarker.longitude),
-                15.0),
-            hideToolbar: false,
-            title: "Choose start position"),
-        toolbarActions: [
-          new ToolbarAction("❌", 1),
-          new ToolbarAction("✔️", 2)
-        ]);
-
-    // React on map ready
-    StreamSubscription sub = mapView.onMapReady.listen((_) {
-      mapView.addMarker(locationMarker);
-    });
-    _subs.add(sub);
-
-    // React on toolbar buttons
-    sub = mapView.onToolbarAction.listen((id) {
-      _hideMap(id);
-    });
-    _subs.add(sub);
-  }
-
-  _hideMap(int id) async {
-    if (id == 1) {
-      if (onCancel != null) {
-        onCancel();
-      }
-    } else if (id == 2) {
-      if (onSubmit != null) {
-        onSubmit(locationMarker.latitude, locationMarker.longitude);
-      }
-    }
-    mapView.dismiss();
-    _subs.cancel();
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new RichText(
+              maxLines: 1,
+              overflow: TextOverflow.clip,
+              text: new TextSpan(
+                text: "Choose a point",
+                style: theme.textTheme.title,
+              ),
+            ),
+            new RichText(
+              maxLines: 1,
+              overflow: TextOverflow.clip,
+              text: new TextSpan(
+                text: "Tap on map to choose",
+                style: theme.textTheme.subhead,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        child: new MapControllerPage(
+          position: position,
+          onSelected: (point) {
+            Navigator.pop(context, point);
+          },
+        ),
+      ),
+    );
   }
 }
