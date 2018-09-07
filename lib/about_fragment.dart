a173ef5a42251cdf2aa0c6d2169fb1276cda36e5
andreashelms <helms@ubilabs.net>
2018-09-07 17:39:36 +0200

    introduces sidebar

---
+++ lib/about_fragment.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FirstFragment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: new Text("It's all about Trufi."),
+    );
+  }
+}
