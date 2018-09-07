9941c1c18de9d0dd10f0fba306680eba276f8eb9
Raimund Wege <raimundwege@gmail.com>
2018-09-07 18:06:50 +0200

    Extract code into generic locations bloc

--- lib/blocs/favorite_locations_bloc.dart
+++ lib/blocs/favorite_locations_bloc.dart
@@ -1,76 +1,18 @@
 import 'dart:async';
-import 'dart:io';
 
-import 'package:rxdart/rxdart.dart';
-
-import 'package:trufi_app/blocs/bloc_provider.dart';
+import 'package:trufi_app/blocs/locations_bloc.dart';
 import 'package:trufi_app/trufi_models.dart';
-import 'package:trufi_app/location/location_storage.dart';
 
-class FavoriteLocationsBloc implements BlocBase {
-  FavoriteLocationsBloc() {
-    _addFavoriteController.listen(_handleAddFavorite);
-    _removeFavoriteController.listen(_handleRemoveFavorite);
-    _init();
-  }
+class FavoriteLocationsBloc extends LocationsBloc {
+  FavoriteLocationsBloc() : super("location_search_favorites.json");
 
-  LocationStorage _favorites;
+  Sink<TrufiLocation> get inAddFavorite => inAddLocation;
 
-  void _init() async {
-    File file = await localFile("location_search_favorites.json");
-    List<TrufiLocation> locations = await readStorage(file);
-    _favorites = LocationStorage(file, locations);
-    _notify();
-  }
+  Sink<TrufiLocation> get inRemoveFavorite => inRemoveLocation;
 
-  // AddFavorite
-  BehaviorSubject<TrufiLocation> _addFavoriteController =
-      new BehaviorSubject<TrufiLocation>();
+  Stream<List<TrufiLocation>> get outFavorites => outLocations;
 
-  Sink<TrufiLocation> get inAddFavorite => _addFavoriteController.sink;
-
-  // RemoveFavorite
-  BehaviorSubject<TrufiLocation> _removeFavoriteController =
-      new BehaviorSubject<TrufiLocation>();
-
-  Sink<TrufiLocation> get inRemoveFavorite => _removeFavoriteController.sink;
-
-  // Favorites
-  BehaviorSubject<List<TrufiLocation>> _favoritesController =
-      new BehaviorSubject<List<TrufiLocation>>(seedValue: []);
-
-  Sink<List<TrufiLocation>> get _inFavorites => _favoritesController.sink;
-
-  Stream<List<TrufiLocation>> get outFavorites => _favoritesController.stream;
-
-  // Dispose
-
-  @override
-  void dispose() {
-    _addFavoriteController.close();
-    _removeFavoriteController.close();
-    _favoritesController.close();
-  }
-
-  // Handle
-
-  void _handleAddFavorite(TrufiLocation value) {
-    _favorites.add(value);
-    _notify();
-  }
-
-  void _handleRemoveFavorite(TrufiLocation value) {
-    _favorites.remove(value);
-    _notify();
-  }
-
-  void _notify() {
-    _inFavorites.add(favorites);
-  }
-
-  // Getter
-
-  List<TrufiLocation> get favorites => _favorites.unmodifiableListView;
+  List<TrufiLocation> get favorites => locations;
 }
 
 int sortByFavorite(
@@ -78,7 +20,5 @@ int sortByFavorite(
   TrufiLocation b,
   List<TrufiLocation> favorites,
 ) {
-  bool aIsFavorite = favorites.contains(a);
-  bool bIsFavorite = favorites.contains(b);
-  return aIsFavorite == bIsFavorite ? 0 : aIsFavorite ? -1 : 1;
+  return sortByLocations(a, b, favorites);
 }
