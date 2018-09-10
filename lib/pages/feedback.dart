d01745ca353df28a9f9400e06d619ff98574d679
Raimund Wege <raimundwege@gmail.com>
2018-09-10 11:15:53 +0200

    Added routes

---
+++ lib/pages/feedback.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:trufi_app/drawer.dart';
+import 'package:trufi_app/trufi_localizations.dart';
+
+class FeedbackPage extends StatelessWidget {
+  static const String route = "feedback";
+
+  @override
+  Widget build(BuildContext context) {
+    TrufiLocalizations localizations = TrufiLocalizations.of(context);
+    return Scaffold(
+      appBar: AppBar(title: Text(localizations.feedback)),
+      body: Center(child: Text("Any questions?")),
+      drawer: buildDrawer(context, route),
+    );
+  }
+}
