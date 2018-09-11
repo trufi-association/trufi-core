d1ad3274787949a091b5571fad8ec92f8aa2b53f
Natalya Blanco <blanco@ubilabs.net>
2018-09-11 14:49:20 +0200

    adds dependency url_launcher to start email app

--- lib/pages/feedback.dart
+++ lib/pages/feedback.dart
@@ -1,7 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'package:trufi_app/drawer.dart';
 import 'package:trufi_app/trufi_localizations.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class FeedbackPage extends StatelessWidget {
   static const String route = "feedback";
@@ -11,8 +14,65 @@ class FeedbackPage extends StatelessWidget {
     TrufiLocalizations localizations = TrufiLocalizations.of(context);
     return Scaffold(
       appBar: AppBar(title: Text(localizations.feedback)),
-      body: Center(child: Text("Any questions?")),
+      body: new _FeedBackPage(),
       drawer: buildDrawer(context, route),
+    );
+  }
+}
+
+class _FeedBackPage extends StatefulWidget {
+  _FeedBackPage({Key key}) : super(key: key);
+
+  @override
+  _FeedBackPageState createState() => new _FeedBackPageState();
+}
+
+class _FeedBackPageState extends State<_FeedBackPage> {
+  Future<Null> _launched;
+
+  Future<Null> _launch(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  Widget _launchStatus(BuildContext context, AsyncSnapshot<Null> snapshot) {
+    if (snapshot.hasError) {
+      return new Text('Error: ${snapshot.error}');
+    } else {
+      return const Text('');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TrufiLocalizations localizations = TrufiLocalizations.of(context);
+    const String toLaunch = 'mailto:feedback@trufi.com?subject=Feedback';
+    return new Center(
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Container(
+            child: new Text(
+              localizations.feedbackContent,
+              maxLines: 3,
+              style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.7),
+            ),
+            padding: const EdgeInsets.all(24.0),
+          ),
+          const Padding(padding: const EdgeInsets.all(16.0)),
+          new RaisedButton(
+            onPressed: () => setState(() {
+                  _launched = _launch(toLaunch);
+                }),
+            child: new Text(localizations.feedbackButton),
+          ),
+          const Padding(padding: const EdgeInsets.all(16.0)),
+          new FutureBuilder<Null>(future: _launched, builder: _launchStatus),
+        ],
+      ),
     );
   }
 }
