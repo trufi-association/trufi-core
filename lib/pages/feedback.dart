5bced142e7e461bdadad1f7cfa72a299a62088ad
Natalya Blanco <blanco@ubilabs.net>
2018-09-12 10:22:22 +0200

    removes wrapper from FeedbackPage

--- lib/pages/feedback.dart
+++ lib/pages/feedback.dart
@@ -5,28 +5,16 @@ import 'package:trufi_app/drawer.dart';
 import 'package:trufi_app/trufi_localizations.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class FeedbackPage extends StatelessWidget {
+class FeedbackPage extends StatefulWidget {
   static const String route = "feedback";
 
-  @override
-  Widget build(BuildContext context) {
-    TrufiLocalizations localizations = TrufiLocalizations.of(context);
-    return Scaffold(
-      appBar: AppBar(title: Text(localizations.feedback)),
-      body: new _FeedBackPage(),
-      drawer: buildDrawer(context, route),
-    );
-  }
-}
-
-class _FeedBackPage extends StatefulWidget {
-  _FeedBackPage({Key key}) : super(key: key);
+  FeedbackPage({Key key}) : super(key: key);
 
   @override
-  _FeedBackPageState createState() => new _FeedBackPageState();
+  FeedBackPageState createState() => new FeedBackPageState();
 }
 
-class _FeedBackPageState extends State<_FeedBackPage> {
+class FeedBackPageState extends State<FeedbackPage> {
   Future<Null> _launched;
 
   Future<Null> _launch(String url) async {
@@ -49,30 +37,33 @@ class _FeedBackPageState extends State<_FeedBackPage> {
   Widget build(BuildContext context) {
     TrufiLocalizations localizations = TrufiLocalizations.of(context);
     const String toLaunch = 'mailto:feedback@trufi.com?subject=Feedback';
-    return new Center(
-      child: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          new Container(
-            child: new Text(
-              localizations.feedbackContent,
-              maxLines: 3,
-              style:
-                  DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.7),
+    return Scaffold(
+      appBar: AppBar(title: Text(localizations.feedback)),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Container(
+              child: Text(
+                localizations.feedbackContent,
+                style: TextStyle(fontSize: 20.0),
+                textAlign: TextAlign.justify,
+              ),
+              padding: const EdgeInsets.all(24.0),
             ),
-            padding: const EdgeInsets.all(24.0),
-          ),
-          const Padding(padding: const EdgeInsets.all(16.0)),
-          new RaisedButton(
-            onPressed: () => setState(() {
-                  _launched = _launch(toLaunch);
-                }),
-            child: new Text(localizations.feedbackButton),
-          ),
-          const Padding(padding: const EdgeInsets.all(16.0)),
-          new FutureBuilder<Null>(future: _launched, builder: _launchStatus),
-        ],
+            const Padding(padding: const EdgeInsets.all(16.0)),
+            new RaisedButton(
+              onPressed: () => setState(() {
+                    _launched = _launch(toLaunch);
+                  }),
+              child: new Text(localizations.feedbackButton),
+            ),
+            const Padding(padding: const EdgeInsets.all(16.0)),
+            new FutureBuilder<Null>(future: _launched, builder: _launchStatus),
+          ],
+        ),
       ),
+      drawer: buildDrawer(context, FeedbackPage.route),
     );
   }
 }
