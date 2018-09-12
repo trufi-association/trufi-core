90ed70ad9775486e0483c17a1382b828ffe858b9
Natalya Blanco <blanco@ubilabs.net>
2018-09-12 14:47:25 +0200

    Adds padding and TeamPage

---
+++ lib/pages/team.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:trufi_app/drawer.dart';
+import 'package:trufi_app/trufi_localizations.dart';
+
+class TeamPage extends StatelessWidget {
+  static const String route = "team";
+
+  @override
+  Widget build(BuildContext context) {
+    TrufiLocalizations localizations = TrufiLocalizations.of(context);
+    return Scaffold(
+      appBar: AppBar(title: Text(localizations.menuTeam)),
+      body: Center(
+        child: Text('Team!'),
+      ),
+      drawer: buildDrawer(context, route),
+    );
+  }
+}
