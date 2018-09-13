9e5729f43aed3d581eab966cbb09cb99315a35bd
Natalya Blanco <blanco@ubilabs.net>
2018-09-13 11:43:15 +0200

    adds padding to the form field

--- lib/location/location_form_field.dart
+++ lib/location/location_form_field.dart
@@ -7,12 +7,10 @@ class LocationFormField extends FormField<TrufiLocation> {
   LocationFormField({
     Key key,
     FormFieldSetter<TrufiLocation> onSaved,
-    TrufiLocation initialValue,
     String hintText,
   }) : super(
           key: key,
           onSaved: onSaved,
-          initialValue: initialValue,
           builder: (FormFieldState<TrufiLocation> state) {
             ThemeData theme = Theme.of(state.context);
             TextStyle textStyle = theme.textTheme.body1;
@@ -36,23 +34,25 @@ class LocationFormField extends FormField<TrufiLocation> {
                     color: Colors.transparent,
                     border: new Border.all(color: Colors.black, width: 1.0),
                     borderRadius: BorderRadius.all(Radius.circular(8.0)),
-
                   ),
                   child: SizedBox(
                     height: 32.0,
                     child: FittedBox(
                       fit: BoxFit.scaleDown,
                       alignment: Alignment.centerLeft,
-                      child: RichText(
-                        text: state.value != null
-                            ? TextSpan(
-                                style: textStyle,
-                                text: state.value.description,
-                              )
-                            : TextSpan(
-                                style: hintStyle,
-                                text: hintText,
-                              ),
+                      child: Padding(
+                        padding: EdgeInsets.all(4.0),
+                        child: RichText(
+                          text: state.value != null
+                              ? TextSpan(
+                                  style: textStyle,
+                                  text: state.value.description,
+                                )
+                              : TextSpan(
+                                  style: hintStyle,
+                                  text: hintText,
+                                ),
+                        ),
                       ),
                     ),
                   ),
