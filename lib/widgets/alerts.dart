1d4dc43d2566304ddd04cd29156f2fb1bd7bdc91
andreashelms <helms@ubilabs.net>
2018-09-14 11:42:37 +0200

    adds error alert popups

--- lib/widgets/alerts.dart
+++ lib/widgets/alerts.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
-
 import 'package:trufi_app/trufi_localizations.dart';
 
 Widget buildAlertLocationServicesDenied(BuildContext context) {
   TrufiLocalizations localizations = TrufiLocalizations.of(context);
+  return buildAlert(context, localizations.alertLocationServicesDeniedTitle,
+      localizations.alertLocationServicesDeniedMessage);
+}
+
+Widget buildAlert(BuildContext context, String title, String text) {
+  TrufiLocalizations localizations = TrufiLocalizations.of(context);
   return AlertDialog(
-      title: Text(localizations.alertLocationServicesDeniedTitle),
-      content: Text(localizations.alertLocationServicesDeniedMessage),
+      title: Text(title),
+      content: Text(text),
       actions: <Widget>[
         FlatButton(
           onPressed: () => Navigator.pop(context),
