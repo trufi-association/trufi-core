93a354a7785e334e5f7c5e586c323e92d692500e
Raimund Wege <raimundwege@gmail.com>
2018-09-17 12:14:39 +0200

    Clean up map

--- lib/widgets/trufi_map.dart
+++ lib/widgets/trufi_map.dart
@@ -7,15 +7,19 @@ import 'package:trufi_app/blocs/location_provider_bloc.dart';
 import 'package:trufi_app/trufi_map_utils.dart';
 
 class TrufiMap extends StatefulWidget {
+  static final LatLng cochabambaLocation = LatLng(-17.3940469, -66.233916);
+
   TrufiMap({
     @required this.mapController,
     @required this.mapOptions,
     this.layers,
+    this.initialPosition,
   });
 
   final MapController mapController;
   final MapOptions mapOptions;
   final List<LayerOptions> layers;
+  final LatLng initialPosition;
 
   @override
   TrufiMapState createState() => TrufiMapState();
@@ -23,6 +27,20 @@ class TrufiMap extends StatefulWidget {
 
 class TrufiMapState extends State<TrufiMap> {
   Marker yourLocationMarker;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.mapController.onReady.then((_) {
+      widget.mapController.move(
+        widget.initialPosition != null
+            ? widget.initialPosition
+            : TrufiMap.cochabambaLocation,
+        12.0,
+      );
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
