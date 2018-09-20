6cd531b36611d1ef94c76406bde3daffaaca45e4
Natalya Blanco <blanco@ubilabs.net>
2018-09-20 11:35:48 +0200

    adds mockito dependency

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,24 +6,19 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:trufi_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new TrufiApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Trufi App - Home Widget', (WidgetTester tester) async {
+    await tester.pumpWidget(TrufiApp());
+    HomeRobot().seesMyLocationFab();
   });
+}
+
+class HomeRobot{
+  HomeRobot seesMyLocationFab(){
+    expect(find.byIcon(Icons.my_location), findsOneWidget);
+    return this;
+  }
+
 }
