1e54e30b58749dd1a8d84ac55e1a85fd4d7255b6
Raimund Wege <raimundwege@gmail.com>
2018-09-24 15:15:31 +0200

    Added more robot actions

--- test_driver/robots/search_robot.dart
+++ test_driver/robots/search_robot.dart
@@ -3,6 +3,7 @@ import 'package:flutter_driver/flutter_driver.dart';
 import 'package:trufi_app/location/location_search_keys.dart' as search_keys;
 
 import 'robot.dart';
+import 'home_robot.dart';
 
 class SearchRobot extends Robot {
   SearchRobot(
@@ -17,5 +18,15 @@ class SearchRobot extends Robot {
   SearchRobot seesSearchField() {
     work = work.then((_) async => await seesTooltip("Search"));
     return this;
+  }
+
+  SearchRobot seesBackButton() {
+    work = work.then((_) async => await seesTooltip(Robot.backButtonTooltip));
+    return this;
+  }
+
+  HomeRobot tapsOnBackButton() {
+    work = work.then((_) async => await tapsOnTooltip(Robot.backButtonTooltip));
+    return HomeRobot(driver, work);
   }
 }
