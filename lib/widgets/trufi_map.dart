513bd09a7cc6179f5aaedba6b6c881d404a2c29b
Raimund Wege <raimundwege@gmail.com>
2018-10-01 11:31:38 +0200

    Switch tiles when offline mode is activated

--- lib/widgets/trufi_map.dart
+++ lib/widgets/trufi_map.dart
@@ -3,6 +3,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong/latlong.dart';
 
 import 'package:trufi_app/blocs/location_provider_bloc.dart';
+import 'package:trufi_app/blocs/preferences_bloc.dart';
 import 'package:trufi_app/trufi_map_utils.dart';
 
 class TrufiMap extends StatefulWidget {
@@ -27,36 +28,53 @@ class TrufiMap extends StatefulWidget {
 }
 
 class TrufiMapState extends State<TrufiMap> {
-  Marker yourLocationMarker;
-
   @override
   Widget build(BuildContext context) {
     final locationProviderBloc = LocationProviderBloc.of(context);
-    return StreamBuilder<LatLng>(
-      stream: locationProviderBloc.outLocationUpdate,
-      builder: (BuildContext context, AsyncSnapshot<LatLng> snapshot) {
-        List<LayerOptions> layers = List();
-        // Map tiles layer
-        layers.add(tileHostingTileLayerOptions());
-        // Parent background layers
-        if (widget.backgroundLayers != null) {
-          layers.addAll(widget.backgroundLayers);
-        }
-        // Your location layer
-        if (snapshot.data != null) {
-          yourLocationMarker = buildYourLocationMarker(snapshot.data);
-          layers.add(MarkerLayerOptions(markers: <Marker>[yourLocationMarker]));
-        } else {
-          yourLocationMarker = null;
-        }
-        // Parent foreground layers
-        if (widget.foregroundLayers != null) {
-          layers.addAll(widget.foregroundLayers);
-        }
-        return FlutterMap(
-          mapController: widget.mapController,
-          options: widget.mapOptions,
-          layers: layers,
+    final preferencesBloc = PreferencesBloc.of(context);
+    return StreamBuilder<bool>(
+      stream: preferencesBloc.outChangeOffline,
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<bool> snapshotOffline,
+      ) {
+        return StreamBuilder<LatLng>(
+          stream: locationProviderBloc.outLocationUpdate,
+          builder: (
+            BuildContext context,
+            AsyncSnapshot<LatLng> snapshotLocation,
+          ) {
+            List<LayerOptions> layers = List();
+            // Map tiles layer
+            layers.add(
+              snapshotOffline.data == true
+                  ? offlineMapTileLayerOptions()
+                  : tileHostingTileLayerOptions(),
+            );
+            // Parent background layers
+            if (widget.backgroundLayers != null) {
+              layers.addAll(widget.backgroundLayers);
+            }
+            // Your location layer
+            if (snapshotLocation.data != null) {
+              layers.add(
+                MarkerLayerOptions(markers: <Marker>[
+                  buildYourLocationMarker(
+                    snapshotLocation.data,
+                  ),
+                ]),
+              );
+            }
+            // Parent foreground layers
+            if (widget.foregroundLayers != null) {
+              layers.addAll(widget.foregroundLayers);
+            }
+            return FlutterMap(
+              mapController: widget.mapController,
+              options: widget.mapOptions,
+              layers: layers,
+            );
+          },
         );
       },
     );
