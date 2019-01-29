72150a7e2b3ce81af0b79bbea2a722a78c896cf7
Samuel Riotz <samuelriotz@gmail.com>
2019-01-29 10:12:04 -0400

    fix null pointer exception

--- lib/location/location_search_storage.dart
+++ lib/location/location_search_storage.dart
@@ -154,10 +154,12 @@ LocationSearchData _parseSearchJson(String encoded) {
               );
             }
           });
+          street1.junctions.sort(
+            (TrufiStreetJunction a, TrufiStreetJunction b) {
+              return a.description.compareTo(b.description);
+            },
+          );
         }
-        street1.junctions.sort((TrufiStreetJunction a, TrufiStreetJunction b) {
-          return a.description.compareTo(b.description);
-        });
       });
       return LocationSearchData(places, streets.values.toList());
     } catch (e) {
