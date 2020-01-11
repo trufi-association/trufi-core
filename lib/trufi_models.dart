f87e50b721bd04ee4bae12ea7f3ee3c362a1e214
GustavoTch <GustavoTitoChoque1358923@gmail.com>
2020-01-11 00:15:57 -0400

    fix:We need to save the location type, for keep it

--- lib/trufi_models.dart
+++ lib/trufi_models.dart
@@ -22,7 +22,8 @@ class TrufiLocation {
   static const String keyDescription = 'description';
   static const String keyLatitude = 'latitude';
   static const String keyLongitude = 'longitude';
-
+  static const String keyType = 'type';
+  
   final String description;
   final double latitude;
   final double longitude;
@@ -81,6 +82,7 @@ class TrufiLocation {
       description: json[keyDescription],
       latitude: json[keyLatitude],
       longitude: json[keyLongitude],
+      type: json[keyType],
     );
   }
 
@@ -89,6 +91,7 @@ class TrufiLocation {
       keyDescription: description,
       keyLatitude: latitude,
       keyLongitude: longitude,
+      keyType: type,
     };
   }
 
