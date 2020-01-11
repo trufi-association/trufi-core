95bad9f67e83b8181155432db517e00ae8ffa5f8
GustavoTch <GustavoTitoChoque1358923@gmail.com>
2020-01-11 09:26:42 -0400

    Add bloc for: my places

---
+++ lib/blocs/saved_locations_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../blocs/bloc_provider.dart';
+import '../blocs/locations_bloc_base.dart';
+import '../location/location_storage.dart';
+
+class SavedLocationsBloc extends LocationsBlocBase {
+  static SavedLocationsBloc of(BuildContext context) {
+    return BlocProvider.of<SavedLocationsBloc>(context);
+  }
+
+  SavedLocationsBloc(
+    BuildContext context,
+  ) : super(
+          context,
+          SharedPreferencesLocationStorage("saved_locations"),
+        );
+}
