a6e3fd81f50955c809db194a6246e2aad37325a2
Max Weber <weber.max.1988@live.de>
2021-04-11 11:33:56 +0200

    removed old way to mock network images

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,14 @@
-// This is a basic Flutter widget test.
-// To perform an interaction with a widget in your test, use the WidgetTester utility that Flutter
-// provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
-// find child widgets in the widget tree, read text, and verify that the values of widget properties
-// are correct.
-
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 import 'package:trufi_core/location/location_form_field.dart';
 import 'package:trufi_core/trufi_app.dart';
 import 'package:trufi_core/trufi_configuration.dart';
 
-import 'image_tile.dart';
-import 'mock_http_client.dart';
-
 void main() {
-  setUp(() async {
-    HttpOverrides.global = new TestHttpOverrides({
-      Uri.parse('https://maps.tilehosting.com/styles/positron/5/17/tile.png'):
-          dummyImageData
-    });
-  });
-
   testWidgets('Trufi App - Home Widget', (WidgetTester tester) async {
     final trufiCfg = TrufiConfiguration();
+
     trufiCfg.languages.addAll([
       TrufiConfigurationLanguage(
         languageCode: "en",
@@ -33,14 +17,14 @@ void main() {
       ),
     ]);
 
-    await tester.pumpWidget(TrufiApp(
-      theme: ThemeData(
-        primaryColor: const Color(0xff263238),
-        primaryColorLight: const Color(0xffeceff1),
-        accentColor: const Color(0xffd81b60),
-        backgroundColor: Colors.white,
-      ),
-    ));
+    await mockNetworkImagesFor(() async => await tester.pumpWidget(TrufiApp(
+          theme: ThemeData(
+            primaryColor: const Color(0xff263238),
+            primaryColorLight: const Color(0xffeceff1),
+            accentColor: const Color(0xffd81b60),
+            backgroundColor: Colors.white,
+          ),
+        )));
 
     await tester.pumpAndSettle();
     final Finder formField = find.byType(LocationFormField);
