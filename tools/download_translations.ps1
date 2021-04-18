b35e352ffbdeb5edb415b2eff9625eb55c6dec59
Max Weber <weber.max.1988@live.de>
2021-04-18 14:28:10 +0200

    💬 [Translation] Should overtake the base language if there is no string in lokalise

--- tools/download_translations.ps1
+++ tools/download_translations.ps1
@@ -1,6 +1,7 @@
 lokalise2 file download --unzip-to ./translations --config ./tools/config.yml --format arb `
     --original-filenames=false --bundle-structure locale/trufiapp_%LANG_ISO%.%FORMAT% `
-    --all-platforms --filter-langs='ee,es,fr,it,pt,qu,en,de' --replace-breaks=false
+    --all-platforms --filter-langs='ee,es,fr,it,pt,qu,en,de' --replace-breaks=false `
+    --export-empty-as=base
 
 $source = "./translations/locale"
 $dest = "./translations"
