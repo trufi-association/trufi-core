ba00852d208de82cbf358ae009ad3c1fd7abdb47
GustavoTCh <TGustavoCh@gmail.com>
2021-05-02 01:21:04 -0400

    Support multiples TransportMode

--- lib/entities/plan_entity/plan_itinerary_leg.dart
+++ lib/entities/plan_entity/plan_itinerary_leg.dart
@@ -16,6 +16,11 @@ enum TransportMode {
   tram,
   transit,
   walk,
+  // route name keywords for specific types of transportation
+  trufi,
+  micro,
+  miniBus,
+  lightRail,
 }
 
 class PlanItineraryLeg {
@@ -27,7 +32,10 @@ class PlanItineraryLeg {
     this.distance,
     this.duration,
     this.toName,
-  });
+  }) {
+    transportMode = getTransportMode(mode, optional: routeLongName);
+  }
+
 
   static const _distance = "distance";
   static const _duration = "duration";
@@ -39,13 +47,6 @@ class PlanItineraryLeg {
   static const _routeLongName = "routeLongName";
   static const _to = "to";
 
-  // route name keywords for specific types of transportation
-  static const typeTrufi = "trufi";
-  static const typeMicro = "micro";
-  static const typeMinibus = "minibus";
-  static const typeGondola = "gondola";
-  static const typeLightRail = "light rail";
-
   final String points;
   final String mode;
   final String route;
@@ -53,6 +54,7 @@ class PlanItineraryLeg {
   final double distance;
   final double duration;
   final String toName;
+  TransportMode transportMode;
 
   factory PlanItineraryLeg.fromJson(Map<String, dynamic> json) {
     return PlanItineraryLeg(
@@ -64,21 +66,6 @@ class PlanItineraryLeg {
       duration: json[_duration] as double,
       toName: json[_to][_name] as String,
     );
-  }
-
-  String toInstruction(TrufiLocalization localization) {
-    final StringBuffer sb = StringBuffer();
-    if (mode == 'WALK') {
-      sb.write(localization.instructionWalk(_durationString(localization),
-          _distanceString(localization), _toString(localization)));
-    } else {
-      sb.write(localization.instructionRide(
-          _carTypeString(localization) + (route.isNotEmpty ? " $route" : ""),
-          _durationString(localization),
-          _distanceString(localization),
-          _toString(localization)));
-    }
-    return sb.toString();
   }
 
   Map<String, dynamic> toJson() {
@@ -93,21 +80,93 @@ class PlanItineraryLeg {
     };
   }
 
+  String toInstruction(TrufiLocalization localization) {
+    final StringBuffer sb = StringBuffer();
+    if (transportMode == TransportMode.walk){
+      sb.write(localization.instructionWalk(
+          _durationString(localization), _distanceString(localization), _toString(localization)));
+    } else {
+      sb.write(localization.instructionRide(
+          _carTypeString(localization) + (route.isNotEmpty ? " $route" : ""),
+          _durationString(localization),
+          _distanceString(localization),
+          _toString(localization)));
+    }
+    return sb.toString();
+  }
+
   String _carTypeString(TrufiLocalization localization) {
-    final String carType = routeLongName?.toLowerCase() ?? "";
-    return mode == 'CAR'
-        ? localization.instructionVehicleCar
-        : carType.contains(typeTrufi)
-            ? localization.instructionVehicleTrufi
-            : carType.contains(typeMicro)
-                ? localization.instructionVehicleMicro
-                : carType.contains(typeMinibus)
-                    ? localization.instructionVehicleMinibus
-                    : carType.contains(typeGondola)
-                        ? localization.instructionVehicleGondola
-                        : carType.contains(typeLightRail)
-                            ? localization.instructionVehicleLightRail
-                            : localization.instructionVehicleBus;
+    String carType = "";
+    switch (transportMode) {
+      case TransportMode.airplane:
+        // TODO translate
+        carType = localization.instructionVehicleBus;
+        break;
+        // TODO translate
+      case TransportMode.bicycle:
+        carType = localization.instructionVehicleBus;
+        break;
+      case TransportMode.bus:
+        carType = localization.instructionVehicleBus;
+        break;
+      case TransportMode.cableCar:
+        carType = localization.instructionVehicleGondola;
+        break;
+      case TransportMode.car:
+        carType = localization.instructionVehicleCar;
+        break;
+        // TODO translate
+      case TransportMode.ferry:
+        carType = localization.instructionVehicleBus;
+        break;
+        // TODO translate
+      case TransportMode.flexible:
+        carType = localization.instructionVehicleBus;
+        break;
+      case TransportMode.funicular:
+        carType = localization.instructionVehicleBus;
+        break;
+      case TransportMode.gondola:
+        carType = localization.instructionVehicleGondola;
+        break;
+        // TODO translate
+      case TransportMode.legSwitch:
+        carType = localization.instructionVehicleBus;
+        break;
+      case TransportMode.rail:
+        carType = localization.instructionVehicleLightRail;
+        break;
+        // TODO translate
+      case TransportMode.subway:
+        carType = localization.instructionVehicleLightRail;
+        break;
+        // TODO translate
+      case TransportMode.tram:
+        carType = localization.instructionVehicleLightRail;
+        break;
+      case TransportMode.transit:
+        carType = localization.instructionVehicleBus;
+        break;
+      case TransportMode.walk:
+        carType = localization.instructionVehicleBus;
+        break;
+      // route name keywords for specific types of transportation
+      case TransportMode.trufi:
+        carType = localization.instructionVehicleTrufi;
+        break;
+      case TransportMode.micro:
+        carType = localization.instructionVehicleMicro;
+        break;
+      case TransportMode.miniBus:
+        carType = localization.instructionVehicleMinibus;
+        break;
+      case TransportMode.lightRail:
+        carType = localization.instructionVehicleLightRail;
+        break;
+      default:
+        carType = localization.instructionVehicleBus;
+    }
+    return carType;
   }
 
   String _distanceString(TrufiLocalization localization) {
@@ -125,49 +184,82 @@ class PlanItineraryLeg {
     return toName == 'Destination' ? localization.commonDestination : toName;
   }
 
-  IconData iconData() {
-    final String carType = routeLongName?.toLowerCase() ?? "";
-    return mode == 'WALK'
-        ? Icons.directions_walk
-        : mode == 'CAR'
-            ? Icons.drive_eta
-            : carType.contains(typeTrufi)
-                ? Icons.local_taxi
-                : carType.contains(typeMicro)
-                    ? Icons.directions_bus
-                    : carType.contains(typeMinibus)
-                        ? Icons.airport_shuttle
-                        : carType.contains(typeGondola)
-                            ? CustomIcons.gondola
-                            : carType.contains(typeLightRail)
-                                ? Icons.train
-                                : Icons.directions_bus;
-  }
+  IconData iconData() => transportMode.icon;
 }
 
-TransportMode getTransportMode(String enumText) 
-    => TransportModeExtension.names.keys.firstWhere(
+TransportMode getTransportMode(String enumText, {String optional}) =>
+    TransportModeExtension.names.keys.firstWhere(
       (key) => key.name == enumText,
-      orElse: () => TransportMode.walk,
+      orElse: () => optional != null
+          ? _getSpecificTransport(
+              optional,
+            )
+          : TransportMode.walk,
     );
 
+TransportMode _getSpecificTransport(String enumText) {
+  final String enumType = enumText?.toUpperCase() ?? "";
+  return enumType.contains(TransportMode.trufi.name)
+      ? TransportMode.trufi
+      : enumType.contains(TransportMode.micro.name)
+          ? TransportMode.bus
+          : enumType.contains(TransportMode.miniBus.name)
+              ? TransportMode.miniBus
+              : enumType.contains(TransportMode.gondola.name)
+                  ? TransportMode.gondola
+                  : enumType.contains(TransportMode.lightRail.name)
+                      ? TransportMode.lightRail
+                      : TransportMode.walk;
+}
+
+
+
 extension TransportModeExtension on TransportMode {
-  static const names = {
-    TransportMode.airplane: "AIRPLANE",
-    TransportMode.bicycle: "BICYCLE",
-    TransportMode.bus: "BUS",
-    TransportMode.cableCar: "CABLE_CAR",
-    TransportMode.car: "CAR",
-    TransportMode.ferry: "FERRY",
-    TransportMode.flexible: "FLEXIBLE",
-    TransportMode.funicular: "FUNICULAR",
-    TransportMode.gondola: "GONDOLA",
-    TransportMode.legSwitch: "LEG_SWITCH",
-    TransportMode.rail: "RAIL",
-    TransportMode.subway: "SUBWAY",
-    TransportMode.tram: "TRAM",
-    TransportMode.transit: "TRANSIT",
-    TransportMode.walk: "WALK",
+  static const names = <TransportMode,String>{
+    TransportMode.airplane  : "AIRPLANE",
+    TransportMode.bicycle   : "BICYCLE",
+    TransportMode.bus       : "BUS",
+    TransportMode.cableCar  : "CABLE_CAR",
+    TransportMode.car       : "CAR",
+    TransportMode.ferry     : "FERRY",
+    TransportMode.flexible  : "FLEXIBLE",
+    TransportMode.funicular : "FUNICULAR",
+    TransportMode.gondola   : "GONDOLA",
+    TransportMode.legSwitch : "LEG_SWITCH",
+    TransportMode.rail      : "RAIL",
+    TransportMode.subway    : "SUBWAY",
+    TransportMode.tram      : "TRAM",
+    TransportMode.transit   : "TRANSIT",
+    TransportMode.walk      : "WALK",
+    // route name keywords for specific types of transportation
+    TransportMode.trufi     : "TRUFI",
+    TransportMode.micro     : "MICRO",
+    TransportMode.miniBus   : "MINIBUS",
+    TransportMode.lightRail : "LIGHT RAIL",
+  };
+
+  static const icons = <TransportMode,IconData>{
+    TransportMode.airplane  : Icons.airplanemode_active,
+    TransportMode.bicycle   : Icons.pedal_bike,
+    TransportMode.bus       : Icons.directions_bus,
+    TransportMode.cableCar  : CustomIcons.gondola,
+    TransportMode.car       : Icons.drive_eta,
+    TransportMode.ferry     : Icons.directions_ferry,
+    TransportMode.flexible  : Icons.warning,
+    TransportMode.funicular : CustomIcons.gondola,
+    TransportMode.gondola   : CustomIcons.gondola,
+    TransportMode.legSwitch : Icons.warning,
+    TransportMode.rail      : Icons.train,
+    TransportMode.subway    : Icons.directions_subway,
+    TransportMode.tram      : Icons.warning,
+    TransportMode.transit   : Icons.directions_transit,
+    TransportMode.walk      : Icons.directions_walk,
+    // route icons for specific types of transportation
+    TransportMode.trufi     : Icons.local_taxi,
+    TransportMode.micro     : Icons.directions_bus,
+    TransportMode.miniBus   : Icons.airport_shuttle,
+    TransportMode.lightRail : Icons.train,
   };
   String get name => names[this] ?? 'WALK';
+  IconData get icon => icons[this] ?? Icons.directions_walk;
 }
