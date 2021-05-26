428e966f7c0db1af15c9fa551b6ce4f47134e917
GustavoTCh <TGustavoCh@gmail.com>
2021-05-26 05:48:05 +0200

    [Feature] adding bottomBarTheme

--- lib/models/theme_state.dart
+++ lib/models/theme_state.dart
@@ -4,13 +4,14 @@ import 'package:flutter/material.dart';
 class ThemeState extends Equatable {
   final ThemeData activeTheme;
   final ThemeData searchTheme;
+  final ThemeData bottomBarTheme;
 
-  const ThemeState({this.activeTheme, this.searchTheme});
+  const ThemeState({this.activeTheme, this.searchTheme, this.bottomBarTheme});
 
   @override
-  List<Object> get props => [activeTheme, searchTheme];
+  List<Object> get props => [activeTheme, searchTheme, bottomBarTheme];
 
   @override
   String toString() =>
-      "ThemeState: {activeTheme $activeTheme, searchTheme $searchTheme}";
+      "ThemeState: {activeTheme $activeTheme, searchTheme $searchTheme}, bottomBarTheme $bottomBarTheme}";
 }
