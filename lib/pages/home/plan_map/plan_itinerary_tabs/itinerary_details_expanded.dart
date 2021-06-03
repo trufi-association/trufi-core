73db55dd0d631c145b444b616e2ece4fd3390f4c
Max <weber.max.1988@live.de>
2021-06-03 14:27:57 +0200

    [Cleanup] renamed folder because of typo

--- lib/pages/home/plan_map/plan_itinerary_tabs/itinerary_details_expanded.dart
+++ lib/pages/home/plan_map/plan_itinerary_tabs/itinerary_details_expanded.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:trufi_core/entities/ad_entity/ad_entity.dart';
 import 'package:trufi_core/entities/plan_entity/plan_entity.dart';
+import 'package:trufi_core/pages/home/plan_map/plan_itinerary_tabs/itinerary_details_expanded/leg_overview_advanced/leg_overview_advanced.dart';
 
 import 'itinerary_details_expanded/leg_overview.dart';
-import 'itinerary_details_expanded/leg_overview_advenced/leg_overview_advanced.dart';
 
 class ItineraryDetailsExpanded extends StatelessWidget {
   final Animation<double> animationDetailHeight;
