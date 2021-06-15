c8f46b57b346572bbf33a4b8390ca42f72f0ce6a
GustavoTCh <TGustavoCh@gmail.com>
2021-06-15 14:07:42 +0200

    [Feature] remove number itineraries

--- lib/pages/home/plan_map/mode_transport_screen.dart
+++ lib/pages/home/plan_map/mode_transport_screen.dart
@@ -61,13 +61,7 @@ class _ModeTransportScreenState extends State<ModeTransportScreen>
   Widget build(BuildContext context) {
     final cfg = context.read<ConfigurationCubit>().state;
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            Text('${widget.title} (${widget.plan.itineraries.length})')
-          ],
-        ),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: CustomScrollableContainer(
         panelMinSize: MediaQuery.of(context).size.height / 3,
         bodyMinSize: MediaQuery.of(context).size.height / 3,
