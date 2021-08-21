b235028c5d0bc23f3b548f0ca83fd10d0620c320
dileep kumar <63330310+dileep9490@users.noreply.github.com>
2021-08-21 14:50:51 +0530

    Created bus model

--- lib/blocs/busses_info/bus_state.dart
+++ lib/blocs/busses_info/bus_state.dart
@@ -1,20 +1,49 @@
 part of 'bus_cubit.dart';
 
 class Bus {
-  String name;
-  int id;
-  String from;
-  String to;
+  Properties properites;
+  Geometry geometry;
+  List nodes;
+  Map<String, dynamic> busdata;
 
 //create a bus data
 
-  Bus({this.id, this.name, this.from, this.to});
+  Bus({this.properites, this.geometry, this.nodes});
 
-  factory Bus.fromJson(Map<String, dynamic> json) {
-    return Bus(
-        name: json["name"] as String,
-        from: json["from"] as String,
+  factory Bus.fromJson(Map<String, dynamic> json) => Bus(
+      properites:
+          Properties.fromjson(json["properties"] as Map<String, dynamic>),
+      geometry: Geometry.forJson(json["geometry"] as Map<String, dynamic>),
+      nodes: json["nodes"] as List);
+}
+
+class Geometry {
+  String type = "LineString";
+  List coords = [];
+  Geometry({this.coords});
+  factory Geometry.forJson(Map<String, dynamic> json) =>
+      Geometry(coords: json["coordinates"] as List);
+}
+
+class Properties {
+  String name;
+  String modeOfTransport;
+  int id;
+  String stroke;
+  int strokeWidth;
+
+  Properties(
+      {this.id,
+      this.name,
+      this.modeOfTransport,
+      this.stroke,
+      this.strokeWidth});
+
+  factory Properties.fromjson(Map<String, dynamic> json) => Properties(
         id: json["id"] as int,
-        to: json["to"] as String);
-  }
+        name: json["name"] as String,
+        modeOfTransport: json["route"] as String,
+        stroke: json["stroke"] as String,
+        strokeWidth: json["stroke-width"] as int,
+      );
 }
