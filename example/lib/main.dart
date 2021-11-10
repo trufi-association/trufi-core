4cd36c9e9a11c65897860e178cd82c41f4dfd1cb
Gustavo TIto <42806689+GustavoTCh@users.noreply.github.com>
2021-11-09 21:18:46 -0400

    [Fixed] initialize hive

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,12 +4,15 @@ import 'package:trufi_core/models/menu/social_media/facebook_social_media.dart';
 import 'package:trufi_core/models/menu/social_media/instagram_social_media.dart';
 import 'package:trufi_core/models/menu/social_media/twitter_social_media.dart';
 import 'package:trufi_core/models/menu/social_media/website_social_media.dart';
+import 'package:trufi_core/services/plan_request/online_graphql_repository/graphql_client/hive_init.dart';
 import 'package:trufi_core/trufi_app.dart';
 import 'package:trufi_core_example/configuration_service.dart';
 
 import 'theme/theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initHiveForFlutter();
   // Run app
   runApp(TrufiApp(
     configuration: setupExampleConfiguration(),
