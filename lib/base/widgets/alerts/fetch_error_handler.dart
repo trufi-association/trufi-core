2b5f87adb3499e9303488a7f0bb0d992304c061c
Gustavo TIto <42806689+GustavoTCh@users.noreply.github.com>
2022-02-15 17:10:08 -0400

    [Feature] enhanced error

--- lib/base/widgets/alerts/fetch_error_handler.dart
+++ lib/base/widgets/alerts/fetch_error_handler.dart
@@ -22,7 +22,8 @@ Future<dynamic> onFetchError(BuildContext context, dynamic exception) async {
     case FetchOnlinePlanException:
       return showTrufiDialog(
         context: context,
-        builder: (dialogContext) => const BuildTransitErrorAlert(),
+        builder: (dialogContext) =>
+            BuildTransitErrorAlert(exception: exception),
       );
     default:
       return _showErrorAlert(
@@ -44,41 +45,4 @@ Future<void> _showErrorAlert({
       );
     },
   );
-}
-
-String localizedErrorForPlanError(
-  int errorId,
-  String message,
-  TrufiBaseLocalization localization,
-) {
-  if (errorId == 500 || errorId == 503) {
-    return localization.errorServerUnavailable;
-  } else if (errorId == 400) {
-    return localization.errorOutOfBoundary;
-  } else if (errorId == 404) {
-    return localization.errorPathNotFound;
-  } else if (errorId == 406) {
-    return localization.errorNoTransitTimes;
-  } else if (errorId == 408) {
-    return localization.errorServerTimeout;
-  } else if (errorId == 409) {
-    return localization.errorTrivialDistance;
-  } else if (errorId == 413) {
-    return localization.errorServerCanNotHandleRequest;
-  } else if (errorId == 440) {
-    return localization.errorUnknownOrigin;
-  } else if (errorId == 450) {
-    return localization.errorUnknownDestination;
-  } else if (errorId == 460) {
-    return localization.errorUnknownOriginDestination;
-  } else if (errorId == 470) {
-    return localization.errorNoBarrierFree;
-  } else if (errorId == 340) {
-    return localization.errorAmbiguousOrigin;
-  } else if (errorId == 350) {
-    return localization.errorAmbiguousDestination;
-  } else if (errorId == 360) {
-    return localization.errorAmbiguousOriginDestination;
-  }
-  return message;
 }
