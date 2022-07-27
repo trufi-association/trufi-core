8c855ef94c096b295e96626580d6848465440a40
Gustavo T <tgustavoch@gmail.com>
2022-07-26 23:28:55 -0400

    [Feature] add CertificatedLetsencryptAndroid

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:trufi_core/base/blocs/map_configuration/map_configuration_cubit.dart';
 import 'package:trufi_core/base/models/trufi_latlng.dart';
+import 'package:trufi_core/base/utils/certificates_letsencrypt_android.dart';
 import 'package:trufi_core/base/utils/graphql_client/hive_init.dart';
 import 'package:trufi_core/base/widgets/drawer/menu/social_media_item.dart';
 import 'package:trufi_core/default_values.dart';
@@ -9,6 +10,7 @@ import 'package:trufi_core/trufi_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await CertificatedLetsencryptAndroid.workAroundCertificated();
   await initHiveForFlutter();
   runApp(
     TrufiApp(
