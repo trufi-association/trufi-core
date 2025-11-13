e9b2f7a862c8ef6e2d62ed0dd3c849b08c4e9e8e
Gustavo T. <tgustavoch@gmail.com>
2025-11-13 10:41:50 -0400

    [Feat] #64 Jilotepec: Actualizar lista de horarios

---
+++ lib/base/pages/home/widgets/plan_itinerary_tabs/transport_frecuency.dart
@@ -0,0 +1,13 @@
+const Map<String, String> transportFrecuency = {
+  ...jilotepecFrecuency,
+};
+
+const jilotepecFrecuency = {
+  "19525667": "Mo-Su 17:30",
+  "19528584": "Mo-Su 04:00-05:45",
+  "19528664": "Mo-Su 05:20-08:00",
+  "19531729": "Mo-Su 07:25",
+  "19531730": "Mo-Su 10:00",
+  "19531743": "Mo-Su 08:25",
+  "19531744": "Mo-Su 06:40-07:20",
+};
