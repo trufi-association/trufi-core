0c9894274b0aa6f73008c87d90edb66334c4f4d7
Samuel Rioja <samuelriotz@hotmail.com>
2025-12-02 06:40:27 -0400

    refactor: remove widget test for TrufiMapsExampleApp

--- packages/trufi_core_maps/example/test/widget_test.dart
+++ packages/trufi_core_maps/example/test/widget_test.dart
@@ -1,9 +1,2 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:trufi_core_maps_example/main.dart';
-
 void main() {
-  testWidgets('App renders correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(const TrufiMapsExampleApp());
-    expect(find.text('Trufi Core Maps'), findsOneWidget);
-  });
 }
