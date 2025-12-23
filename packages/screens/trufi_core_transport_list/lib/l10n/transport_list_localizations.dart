8ab3aa8bf8a39821bb18940953f58a2133e5bf02
Max Weber <max.weber@sevdesk.de>
2025-12-23 11:19:58 +0100

    feat(628): TransportList is now using the new trnaslation workflow

--- packages/screens/trufi_core_transport_list/lib/l10n/transport_list_localizations.dart
+++ packages/screens/trufi_core_transport_list/lib/l10n/transport_list_localizations.dart
@@ -63,15 +63,20 @@ import 'transport_list_localizations_es.dart';
 /// be consistent with the languages listed in the TransportListLocalizations.supportedLocales
 /// property.
 abstract class TransportListLocalizations {
-  TransportListLocalizations(String locale) : localeName = intl.Intl.canonicalizedLocale(locale.toString());
+  TransportListLocalizations(String locale)
+    : localeName = intl.Intl.canonicalizedLocale(locale.toString());
 
   final String localeName;
 
   static TransportListLocalizations? of(BuildContext context) {
-    return Localizations.of<TransportListLocalizations>(context, TransportListLocalizations);
+    return Localizations.of<TransportListLocalizations>(
+      context,
+      TransportListLocalizations,
+    );
   }
 
-  static const LocalizationsDelegate<TransportListLocalizations> delegate = _TransportListLocalizationsDelegate();
+  static const LocalizationsDelegate<TransportListLocalizations> delegate =
+      _TransportListLocalizationsDelegate();
 
   /// A list of this localizations delegate along with the default localizations
   /// delegates.
@@ -83,18 +88,19 @@ abstract class TransportListLocalizations {
   /// Additional delegates can be added by appending to this list in
   /// MaterialApp. This list does not have to be used at all if a custom list
   /// of delegates is preferred or required.
-  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates = <LocalizationsDelegate<dynamic>>[
-    delegate,
-    GlobalMaterialLocalizations.delegate,
-    GlobalCupertinoLocalizations.delegate,
-    GlobalWidgetsLocalizations.delegate,
-  ];
+  static const List<LocalizationsDelegate<dynamic>> localizationsDelegates =
+      <LocalizationsDelegate<dynamic>>[
+        delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ];
 
   /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
     Locale('de'),
     Locale('en'),
-    Locale('es')
+    Locale('es'),
   ];
 
   /// No description provided for @menuTransportList.
@@ -128,35 +134,40 @@ abstract class TransportListLocalizations {
   String stops(int count);
 }
 
-class _TransportListLocalizationsDelegate extends LocalizationsDelegate<TransportListLocalizations> {
+class _TransportListLocalizationsDelegate
+    extends LocalizationsDelegate<TransportListLocalizations> {
   const _TransportListLocalizationsDelegate();
 
   @override
   Future<TransportListLocalizations> load(Locale locale) {
-    return SynchronousFuture<TransportListLocalizations>(lookupTransportListLocalizations(locale));
+    return SynchronousFuture<TransportListLocalizations>(
+      lookupTransportListLocalizations(locale),
+    );
   }
 
   @override
-  bool isSupported(Locale locale) => <String>['de', 'en', 'es'].contains(locale.languageCode);
+  bool isSupported(Locale locale) =>
+      <String>['de', 'en', 'es'].contains(locale.languageCode);
 
   @override
   bool shouldReload(_TransportListLocalizationsDelegate old) => false;
 }
 
 TransportListLocalizations lookupTransportListLocalizations(Locale locale) {
-
-
   // Lookup logic when only language code is specified.
   switch (locale.languageCode) {
-    case 'de': return TransportListLocalizationsDe();
-    case 'en': return TransportListLocalizationsEn();
-    case 'es': return TransportListLocalizationsEs();
+    case 'de':
+      return TransportListLocalizationsDe();
+    case 'en':
+      return TransportListLocalizationsEn();
+    case 'es':
+      return TransportListLocalizationsEs();
   }
 
   throw FlutterError(
     'TransportListLocalizations.delegate failed to load unsupported locale "$locale". This is likely '
     'an issue with the localizations generation tool. Please file an issue '
     'on GitHub with a reproducible sample app and the gen-l10n configuration '
-    'that was used.'
+    'that was used.',
   );
 }
